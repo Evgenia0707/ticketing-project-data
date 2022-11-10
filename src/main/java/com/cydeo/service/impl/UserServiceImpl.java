3a6a55ebe33c07d27e294a8243e5bd1d81ec274c
Evgenia0707 <4030071@gmail.com>
2022-11-10 14:23:47 -0500

    Ticketing Project Data

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -24,7 +24,6 @@ public class UserServiceImpl implements UserService {
     private final TaskService taskService;
 
 
-
     public UserServiceImpl(UserRepository userRepository, UserMapper userMapper, @Lazy ProjectService projectService, @Lazy TaskService taskService) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
@@ -35,103 +34,78 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<UserDTO> listAllUsers() {
 
-        List<User> userList = userRepository.findAllByIsDeletedOrderByFirstNameDesc(false);//deleted fild need be false for return all users(give all users if not deleted)
-//                findAll(Sort.by("firstName"));//return is sort - assign
-        //go db grab user -> convert
-        //findAll (impl-Select * from table) - give info from DB -> need write new query(differ busin logic)
+        List<User> userList = userRepository.findAllByIsDeletedOrderByFirstNameDesc(false);//deleted need be false for return all users(give all users if not deleted)
 
         return userList.stream().map(userMapper::convertToDto).collect(Collectors.toList());//userMap - inject
     }
 
     @Override
     public UserDTO findByUserName(String username) {//find obj in DB
-        User user = userRepository.findByUserNameAndIsDeleted(username, false);//findByUserName - need new (bec will see all delet also)
+        User user = userRepository.findByUserNameAndIsDeleted(username, false);
         return userMapper.convertToDto(user); //return DTO
     }
 
     @Override
     public void save(UserDTO user) {//coming from UI - DTO - need DB (entity) use mapper
-    userRepository.save(userMapper.convertToEntity(user));//get DTO convert to Entity (only 1 obj)
+        userRepository.save(userMapper.convertToEntity(user));
     }
 
 //    @Override//we dont hard delete- dont use ()
 //    public void deleteByUserName(String username) {
-//    userRepository.deleteByUserName(username);//add (public void delete(String username)) del in UI not DB
+//    userRepository.deleteByUserName(username);
 //    }
 
     @Override
-    public UserDTO update(UserDTO user) {//(UserDTO user) updated obj -- user coming from UI
+    public UserDTO update(UserDTO user) {
 
-        //(mistake - save second obj , create another primary key) no i
-//        userRepository.save(userMapper.convertToEntity(user));//get user- convert-save
-//        return findByUserName(user.getUserName());
-
-        //find current user - go to DB //get user from db + update user form UI
+        //find current user - go to DB
         //                                       //admin@admin.com23--null(dont have it in DB) -- null.getId
-        User user1 = userRepository.findByUserNameAndIsDeleted(user.getUserName(), false);//has id //findByUserName(need new with condit where delet) UserEntity
+        User user1 = userRepository.findByUserNameAndIsDeleted(user.getUserName(), false);
         //Map update user DTO to Entity obj - save id DB - first covert to Entity
-        User convertedUser = userMapper.convertToEntity(user);// no id
-//        user1.setUserName(user.getUserName());//for convertDTO-Entity if don't have mapper
-
+        User convertedUser = userMapper.convertToEntity(user);
         //set id to the converted object
         convertedUser.setId(user1.getId());
         //save updated user in the DB
         userRepository.save(convertedUser);
-
-        return findByUserName(user.getUserName());//created () with return userDto.
-        //we have ()findByUserName - use it -- User user = userRepository.findByUserName(username);
-        //        return userMapper.convertToDto(user); //return DTO
+        return findByUserName(user.getUserName());
     }
 
     @Override
     public void delete(String username) {//also need update Controller
-        //go to DB and get that user with username
-                //change the isDeleted field to true
-        //save the object in the DB
+        //go to DB and get that user with username/save the object in the DB
         User user = userRepository.findByUserNameAndIsDeleted(username, false);
 
-        //check if assign task- if true - delete - if not - do nothing
-
-        if (checkIfUserCanBeDeleted(user)){
+        if (checkIfUserCanBeDeleted(user)) {
             user.setIsDeleted(true);
-            user.setUserName(user.getUserName() + "-" + user.getId());//create - harold...com-2 if delete
+            user.setUserName(user.getUserName() + "-" + user.getId());
             userRepository.save(user);
         }
-
-           }
+    }
 
     @Override
     public List<UserDTO> listAllByRole(String role) {
         //go to DB bring all users with specif role(manager)
         List<User> users = userRepository.findByRoleDescriptionIgnoreCaseAndIsDeleted(role, false);
         //convert
-
         return users.stream().map(userMapper::convertToDto).collect(Collectors.toList());
     }
 
-    private boolean checkIfUserCanBeDeleted(User user){// user Entity (can pass DTO or Entiry- private() not connect with Controller don have get/send info to Controller)
-
-        //want to know if user manager or employ(have same unopleted project/task)
-        switch (user.getRole().getDescription()){
-
-            case "Manager"://check uncompl proj
-                List<ProjectDTO> projectDTOList = projectService.listAllNonCompletedByAssignedManager(userMapper.convertToDto(user));   //creat() nonComp
-                //need sent dto obj to projService
+    private boolean checkIfUserCanBeDeleted(User user) {
+        //want to know if user manager or employ
+        switch (user.getRole().getDescription()) {
+            case "Manager"://check uncompleted proj
+                List<ProjectDTO> projectDTOList = projectService.listAllNonCompletedByAssignedManager(userMapper.convertToDto(user));                  //need sent dto obj to projService
                 //get nonComlp proj -
-                return projectDTOList.size() == 0;//if 0 - delete(nothing uncompleted)
+                return projectDTOList.size() == 0;//if 0 - delete(all completed)
 
             case "Employee":
-                List<TaskDTO> taskDTOList = taskService.listAllNonCompletedByAssignedEmployee(userMapper.convertToDto(user));   //creat() nonComp
+                List<TaskDTO> taskDTOList = taskService.listAllNonCompletedByAssignedEmployee(userMapper.convertToDto(user));
                 //need sent dto obj to projService
-                //get nonComlp proj -
-                return taskDTOList.size() == 0;//if 0 - delete(nothing uncompleted)
+                return taskDTOList.size() == 0;
 
             default:
                 return true;
         }
     }
-
-
-
 
 }
