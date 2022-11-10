401f3c3e78d3aede22b6a30418afca726c6dffb2
Evgenia0707 <4030071@gmail.com>
2022-11-10 14:08:00 -0500

    Ticketing Project Data

---
+++ src/main/java/com/cydeo/repository/ProjectRepository.java
@@ -0,0 +1,20 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Project;
+import com.cydeo.entity.User;
+import com.cydeo.enums.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+//@Repository// for old version Spring
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+
+    Project findByProjectCode(String code);
+
+    List<Project> findAllByAssignedManager(User manager);//DB give all proj assigned by this manager(User manager)
+
+    List<Project> findAllByProjectStatusIsNotAndAssignedManager(Status status, User assignedManager);
+}
