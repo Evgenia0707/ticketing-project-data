cdf9b97eb8c3e6cd5213dfcd71b01bef0af5ec3e
Evgenia0707 <4030071@gmail.com>
2022-11-10 14:03:26 -0500

    Ticketing Project Data

---
+++ src/main/java/com/cydeo/entity/User.java
@@ -0,0 +1,36 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Gender;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+
+public class User extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true)
+    private String userName;
+    private String passWord;
+    private boolean enabled;
+    private String phone;
+
+    @ManyToOne
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+}
